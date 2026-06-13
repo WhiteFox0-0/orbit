d8edce904d326f791297663da198cd0e1d3be4ad
WhiteFox0-0 <swayammmm0@gmail.com>
2026-06-13 18:55:26 +0530

    feat: add polar subscription

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,3 +1,5 @@
+import { polar, checkout, portal } from "@polar-sh/better-auth"
+import { polarClient } from "./polar";
 import { betterAuth } from "better-auth"
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import prisma from "@/lib/db";
@@ -11,4 +13,24 @@ export const auth = betterAuth({
     enabled: true,
     autoSignIn: true,
   },
+
+  plugins: [
+    polar({
+      client: polarClient,
+      createCustomerOnSignUp: true,
+      use: [
+        checkout({
+          products: [
+            {
+              productId: "eca8ae86-21e7-4d9f-97af-dee8d9cbde7e",
+              slug: "Pro"
+            }
+          ],
+          successUrl: process.env.POLAR_SUCCESS_URL,
+          authenticatedUsersOnly: true,
+        }),
+        portal(),
+      ]
+    })
+  ]
 });
